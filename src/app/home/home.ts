d4e752cb3beecd462a37d20dd711d9a53d6eecef
raghav sharma <raghav@thinkpad.>
2025-09-29 12:23:48 +0000

    fix bg image

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DOCUMENT, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  baseHref: string;
 
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    // This will get the <base href="..."> value from index.html
+    this.baseHref = this.document.getElementsByTagName('base')[0]?.href || '/';
+  }
 }
