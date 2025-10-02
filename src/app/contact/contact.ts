59eb1b5f2c8d9b0ceaafd59648a0077bb2ca1fa4
raghav sharma <dev.raghavsharma@gmail.com>
2025-10-02 10:53:23 +0000

    enable contact form

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
 
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  contactForm = this.formBuilder.group({
+    fullName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    subject: ['', Validators.required],
+    message: ['', Validators.required]
+  });
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted!', this.contactForm.value);
+      // Here you can handle the form submission, e.g., send the data to a server
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
